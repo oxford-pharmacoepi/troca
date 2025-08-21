f482292b46133dc8d14a566480120ff708cf9697
catalamarti <marticatalasabate@gmail.com>
2025-08-21 18:21:50 +0100

    Start troca

---
+++ R/troca.R
@@ -0,0 +1,80 @@
+
+#' Create the TROCA shiny app
+#'
+#' @return Opens the shiny app
+#' @export
+#'
+troca <- function() {
+
+  ui <- bslib::page_fluid(
+    shinychat::chat_ui("chat")
+  )
+
+  server <- function(input, output, session) {
+    chat <- ellmer::chat_google_gemini(
+      system_prompt = prompt,
+      api_key = Sys.getenv("GOOGLE_API_KEY")
+    )
+    store <- ragnar::ragnar_store_connect(
+      location = here::here("model", "troca.duckdb")
+    )
+    chat <- ragnar::ragnar_register_tool_retrieve(
+      chat = chat,
+      store = store,
+      top_k = 10
+    )
+
+    shiny::observeEvent(input$chat_user_input, {
+      stream <- chat$stream_async(input$chat_user_input)
+      shinychat::chat_append(id = "chat", response = stream)
+    })
+  }
+
+  shiny::shinyApp(ui, server)
+}
+
+trainModel <- function() {
+  dbdir <- here::here("model", "troca.duckdb")
+  unlink(dbdir, force = TRUE)
+  unlink(paste0(dbdir, ".wal"), force = TRUE)
+
+  store <- ragnar::ragnar_store_create(
+    location = dbdir,
+    embed = \(x) ragnar::embed_ollama(x, model = "mxbai-embed-large"),
+    name = "troca"
+  )
+
+  # read chunks
+  chunks <- documentation |>
+    purrr::map(\(x) {
+      sublinks <- ragnar::ragnar_find_links(x = x$link)
+      sublinks <- sublinks[!basename(sublinks) %in% c("CONTRIBUTING.html", "LICENSE.html")]
+      sublinks
+    }) |>
+    unlist(use.names = FALSE) |>
+    unique() |>
+    purrr::map(\(link) {
+      tryCatch(
+        expr = {
+          cli::cli_inform(c("i" = "Reading information from {.url {link}}"))
+          ragnar::markdown_chunk(ragnar::read_as_markdown(link))
+        },
+        error = function(e) {
+          cli::cli_inform(c("x" = "Failed to read markdown in {.url {link}}"))
+          NULL
+        })
+    }) |>
+    purrr::compact()
+
+  # Embeding information
+  cli::cli_inform(c(i = "Embeding retrieved information."))
+  cli::cli_progress_bar("Embeding", total = length(chunks), type = "tasks")
+  for (k in seq_along(chunks)) {
+    ragnar::ragnar_store_insert(store = store, chunks = chunks[[k]])
+    cli::cli_progress_update()
+  }
+  cli::cli_progress_done()
+
+  # build store index
+  ragnar::ragnar_store_build_index(store = store)
+}
---
+++ data-raw/prompt.R
@@ -0,0 +1,73 @@
+## code to prepare `DATASET` dataset goes here
+prompt <- stringr::str_squish(
+  "
+  You are an expert R programmer and epidemiologist working with OMOP CDM data.
+  Use concise, accurate R examples using OMOPverse packages (e.g. CDMConnector,
+  CohortConstructor).
+
+  Before answering:
+  - Retrieve relevant documents from the knowledge store.
+  - Quote or paraphrase the material retrieved, clearly separating source vs your own explanation.
+  - Include direct links to cited content (e.g. .io documentation pages).
+  - If no relevant information is found, say 'No information available.'
+
+  Only answer if source material is retrieved.
+  "
+)
+
+# documentation to train troca
+documentation <- list(
+  "tidy book" = list(
+    link = "https://oxford-pharmacoepi.github.io/Tidy-R-programming-with-OMOP/"
+  ),
+  "CDMConnector" = list(
+    link = "https://darwin-eu.github.io/CDMConnector/"
+  ),
+  "omopgenerics" = list(
+    link = "https://darwin-eu.github.io/omopgenerics/"
+  ),
+  "CohortConstructor" = list(
+    link = "https://ohdsi.github.io/CohortConstructor/"
+  ),
+  "visOmopResults" = list(
+    link = "https://darwin-eu.github.io/visOmopResults/"
+  ),
+  "PhenotypeR" = list(
+    link = "https://ohdsi.github.io/PhenotypeR/"
+  ),
+  "OmopViewer" = list(
+    link = "https://ohdsi.github.io/OmopViewer/"
+  ),
+  "DrugUtilisation" = list(
+    link = "https://darwin-eu.github.io/DrugUtilisation/"
+  ),
+  "IncidencePrevalence" = list(
+    link = "https://darwin-eu.github.io/IncidencePrevalence/"
+  ),
+  "DrugExposureDiagnostics" = list(
+    link = "https://darwin-eu.github.io/DrugExposureDiagnostics/"
+  ),
+  "MeasurementDiagnostics" = list(
+    link = "https://ohdsi.github.io/MeasurementDiagnostics/"
+  ),
+  "PatientProfiles" = list(
+    link = "https://darwin-eu.github.io/PatientProfiles/"
+  ),
+  "CohortCharacteristics" = list(
+    link = "https://darwin-eu.github.io/CohortCharacteristics/"
+  ),
+  "OmopSketch" = list(
+    link = "https://OHDSI.github.io/OmopSketch/"
+  ),
+  "CodelistGenerator" = list(
+    link = "https://darwin-eu.github.io/CodelistGenerator/"
+  ),
+  "CohortSurvival" = list(
+    link = "https://darwin-eu-dev.github.io/CohortSurvival/"
+  ),
+  "omock" = list(
+    link = "https://ohdsi.github.io/omock/"
+  )
+)
+
+usethis::use_data(prompt, documentation, overwrite = TRUE, internal = TRUE)
