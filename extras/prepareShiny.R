01149e8f8d79f648fea843aa92dfce16a48ab7b3
catalamarti <marticatalasabate@gmail.com>
2025-09-04 17:43:20 +0100

    Update prepareShiny.R

--- extras/prepareShiny.R
+++ extras/prepareShiny.R
@@ -1,7 +1,5 @@
 
-devtools::load_all()
-
-#trainModel(dbdir = here::here("extras", "shiny", "model", "troca.duckdb"))
+#troca:::trainModel(dbdir = here::here("extras", "shiny", "model", "troca.duckdb"))
 writeLines(
   text = paste0("GOOGLE_API_KEY=\"", Sys.getenv("GOOGLE_API_KEY"), "\""),
   con = here::here("extras", "shiny", ".Renviron")
