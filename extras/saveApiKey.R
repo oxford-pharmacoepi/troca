4762169f0af6b83f4f83af9442dca3aa6ae1d79e
catalamarti <marticatalasabate@gmail.com>
2025-09-04 17:37:12 +0100

    add API key via .Renviron

--- extras/saveApiKey.R
+++
@@ -1,3 +0,0 @@
-
-google_key <- Sys.getenv("GOOGLE_API_KEY")
-save(google_key, file = here::here("extras", "shiny", "model", "key.RData"))
--- extras/shiny/app.R
+++ extras/shiny/app.R
@@ -1,6 +1,7 @@
 
 library(troca)
-load(here::here("model", "key.RData"))
+
+print(Sys.getenv("GOOGLE_API_KEY"))
 
 chat <- chat <- ellmer::chat_google_gemini()
 troca(chat = chat)
